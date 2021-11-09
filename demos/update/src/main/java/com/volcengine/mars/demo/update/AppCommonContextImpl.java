a9fb0f4d245773a80f6a11d84c68237122a65301
王江 <wangjiang.666@bytedance.com>
2021-11-09 10:43:26 +0800

    修改下发端地址

--- demos/update/src/main/java/com/volcengine/mars/demo/update/AppCommonContextImpl.java
+++ demos/update/src/main/java/com/volcengine/mars/demo/update/AppCommonContextImpl.java
@@ -16,7 +16,7 @@ public class AppCommonContextImpl extends AbsAppCommonContext {
 
     @Override
     public String getCustomUrl() {
-        return "https://release-tob-api.vemarsdev.com/v1/gray_release/package";
+        return "https://release-api.vemarsdev.com/v1/gray_release/package";
     }
 
 
